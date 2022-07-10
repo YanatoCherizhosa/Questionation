92b554c7ac617e31380d074363185b196d375f3d
YanatoCherizhosa <pigma987@gmail.com>
2022-07-11 01:19:38 +0300

    Quiz maker. HP recover
    Some changes in quiz making. Now questions in different quizes generating randomly. Also when going back in menu HP will restore and new quiz will be generated.

--- Scripts/Game/DataLoad.gd
+++ Scripts/Game/DataLoad.gd
@@ -22,21 +22,11 @@ func _ready():
 	else:
 		GetQuestionsFromDB();
 	
-	#creating random arrays of questions and answers
-	randomize() #randomize seed using a number, based on time
-	for i in massive.size():
-		randomQuestions.append(int(i))
-	randomQuestions.shuffle()
-	for i in 4:
-		randomAnswers.append(int(i))
-	randomAnswers.shuffle()
-	
+	CreateNewQuiz()
+	print("Start quiz: "+str(randomQuestions))
 	#bind signal to buttons
 	for i in answersButtonGroup.get_buttons():
 		i.connect("pressed", self, "answerButtonPressed")
-		
-	#create first question
-	CreateQuestion(qnumber)
 
 func GetQuestionsFromDB():
 	db = SQLite.new()
@@ -58,6 +48,19 @@ func GetQuestionsFromDB():
 			3 : [db.query_result[i]["w3"],1],
 		}
 		massive.append(quest_dict)
+
+func CreateNewQuiz():
+	randomQuestions.clear()
+	randomAnswers.clear()
+	#creating random arrays of questions and answers
+	randomize() #randomize seed using a number, based on time
+	for i in massive.size():
+		randomQuestions.append(int(i))
+	randomQuestions.shuffle()
+	for i in 4:
+		randomAnswers.append(int(i))
+	randomAnswers.shuffle()
+	CreateQuestion(qnumber)
 	
 func CreateQuestion(var randQ):
 	get_node("MarginContainer/VBoxContainer/VBoxQuestion/Question").text = massive[randomQuestions[randQ]]["quest"]
@@ -66,20 +69,18 @@ func CreateQuestion(var randQ):
 		answersButtonGroup.get_buttons()[i].hp_influence = int(massive[randomQuestions[randQ]][randomAnswers[i]][1])
 				
 func answerButtonPressed():
-	#changing HP by button hp_influence value
-	print("HP influence: "+str(answersButtonGroup.get_pressed_button().hp_influence))
-	print("Player bi HP: "+ str(get_node("/root/Global").playerHealth))
 	get_node("/root/Global").playerHealth = get_node("/root/Global").playerHealth - answersButtonGroup.get_pressed_button().hp_influence
-	
-	print("HP influence: "+str(answersButtonGroup.get_pressed_button().hp_influence))
-	print("Player ai HP: "+ str(get_node("/root/Global").playerHealth))
 	get_node("MarginContainer/VBoxContainer/HPBar").value = get_node("/root/Global").playerHealth
-
+	print("Player ai HP: "+ str(get_node("/root/Global").playerHealth))
+	
 	#if we have questions display them, else you win!
 	if qnumber >= randomQuestions.size()-1:
 		print("Победа!")
 		get_node("/root/Global").playerHealth = 3
+		
 		qnumber = 0
+		CreateNewQuiz()
+		print("New quiz: "+str(randomQuestions))
 	else:
 		qnumber+=1
 		print("Next Question :"+str(qnumber))
@@ -88,6 +89,8 @@ func answerButtonPressed():
 			CreateQuestion(qnumber)
 
 func _on_GoBack_button_up():
+	get_node("/root/Global/").playerHealth = 3
 	if get_tree().change_scene("res://Scenes/Menu.tscn") != OK:
 		print("Error occured when trying to switch to the Menu.tscn")
+		
 
